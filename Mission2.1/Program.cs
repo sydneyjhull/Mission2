8ab2d019c17aa5b684c2effd75792225512e2334
Sydney Hull <sydneyjhull@gmail.com>
2023-01-18 15:45:56 -0700

    Add project files.

---
+++ Mission2.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Mission2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        
+        {
+            Random r = new Random();
+            int numRolls;
+
+            Console.WriteLine("Welcome to the dice throwing simulator! \n");
+            Console.WriteLine("How many dice rolls would you like to simulate? \n");
+            numRolls = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each '*' represents 1% of the total number of rolls. \nTotal number of rolls = " + numRolls + '\n');
+
+            int[] array = new int[13]; //array is the array
+
+            for (int i = 0; i < 13; i++)
+            {
+                array[i] = 0; // This is putting a 0 in each spot of the array
+            }
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int roll = r.Next(2,13); //This is making the variable roll = to the random number rolled
+                array[roll]++; // This is adding the roll into the correct spot of the array
+            }
+
+            for (int i = 2; i < 13; i++)
+            {
+                float percent = ( array[i] / (float)numRolls) * 100; // This is taking the number in the array slot and dividing it by the total number of rolls to get the percentage
+                int rounded = Convert.ToInt32(Math.Round(percent)); // rounding the percent
+                string output = "";
+                for (int f = 0; f < rounded; f++)
+                {
+                    output = (output + "*");
+                }
+                Console.WriteLine(i + ": " + output);
+            }
+        }
+    }
+}
